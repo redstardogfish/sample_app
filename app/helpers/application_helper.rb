bc688822c283fb65163f7bed9c5abe1c9a88a8e1
Tony <ptonyweir@gmail.com>
2010-11-25 16:55:25 +1100

    Added layout links with routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,12 @@ module ApplicationHelper
       base_title
     else
       "#{base_title} | #{@title}"
-    end
-    
+    end  
+  end
+  
+  def logo
+    image_tag("logo.png", :alt => "Sample_app",:class => "value", 
+  				  :class => "round"   )
   end
   
 end

40eab926e308089e6cb16a4302e9be7efcfe1d66
Tony <ptonyweir@gmail.com>
2010-11-26 08:35:23 +1100

    Added a Users controllor and layout link tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector( "title",
-        :content => "Ruby on Rails Sample App | Home" )
+        :content => "#{@base_title} | Home" )
     end
     it "should have a non-blank body" do
       get 'home'
@@ -27,7 +27,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector( "title",
-        :content => "Ruby on Rails Sample App | Contact" )
+        :content => "#{@base_title} | Contact" )
     end
     
   end
@@ -40,7 +40,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector( "title",
-        :content => "Ruby on Rails Sample App | About" )
+        :content => "#{@base_title} | About" )
     end
   end
 
@@ -52,7 +52,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector( "title",
-        :content => "Ruby on Rails Sample App | Help" )
+        :content => "#{@base_title} | Help" )
     end
   end
 

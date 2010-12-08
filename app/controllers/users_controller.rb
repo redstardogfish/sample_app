2a781d9d232daa6590435cdb5edd0f17d107d5d5
Tony <ptonyweir@gmail.com>
2010-12-09 10:57:33 +1100

    Can now destroy users (as administrator)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only   => :destroy 
 
   def index
     @title = "All users"
@@ -30,17 +31,20 @@ class UsersController < ApplicationController
   
   def edit
     @title = "Edit user"
-    @user = User.find(params[:id])
   end
   
   def update
-    @title = "Edit user"
-    @user = User.find(params[:id])  
+    @title = "Edit user" 
     if @user.update_attributes(params[:user])  
       redirect_to @user, :flash => {:success => "Profile updated."}
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => {:success => "User destroyed."} 
   end
 
 
@@ -52,6 +56,10 @@ private
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+  def admin_user
+    user = User.find(params[:id])
+    redirect_to(root_path) if (!current_user.admin? || current_user?(user))
   end
 
 end
